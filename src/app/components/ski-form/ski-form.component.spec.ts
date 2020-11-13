a2ffd1d9c2ba5dad28216c5ba38e43ca784add1f
Keegan McCafferty <kmmccafferty96@gmail.com>
2020-11-13 18:15:42 -0500

    chore(deps): upgrade to angular 11

--- src/app/components/ski-form/ski-form.component.spec.ts
+++ src/app/components/ski-form/ski-form.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { SkiFormComponent } from './ski-form.component';
 
@@ -6,11 +6,13 @@ describe('SkiFormComponent', () => {
   let component: SkiFormComponent;
   let fixture: ComponentFixture<SkiFormComponent>;
 
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [SkiFormComponent]
-    }).compileComponents();
-  }));
+  beforeEach(
+    waitForAsync(() => {
+      TestBed.configureTestingModule({
+        declarations: [SkiFormComponent]
+      }).compileComponents();
+    })
+  );
 
   beforeEach(() => {
     fixture = TestBed.createComponent(SkiFormComponent);
